1f3774f8b9a0052ade22266e6c315d3bf9fc8208
Pooh Love <105697603+PoohL0ve@users.noreply.github.com>
2024-05-08 10:36:51 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -31,7 +31,7 @@ def initiate():
       {"name": "E-Class", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[1]},
       {"name": "A4", "type": "SUV", "year": 2023,
-       "car_make":car_make_instances[2]},
+       "car_make": car_make_instances[2]},
       {"name": "A5", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[2]},
       {"name": "A6", "type": "SUV", "year": 2023,
@@ -47,8 +47,7 @@ def initiate():
       {"name": "Camry", "type": "Sedan", "year": 2023,
        "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV", "year": 2023,
-       "car_make":car_make_instances[4]},
-        # Add more CarModel instances as needed
+       "car_make": car_make_instances[4]},
     ]
 
     for data in car_model_data:
